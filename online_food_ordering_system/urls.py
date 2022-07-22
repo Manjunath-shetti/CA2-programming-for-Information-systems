115b9e6d0830a4f2b31f7a9dd2f0d5c242194778
Manjunath Prakash Shetti <108906493+Manjunath-shetti@users.noreply.github.com>
2022-07-23 00:18:24 +0100

    adding URL's for restaurant app
    adding url for restaurant

--- online_food_ordering_system/urls.py
+++ online_food_ordering_system/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', include('pages.urls')),
     url(r'^pages/', include('pages.urls')),
-    
+    url(r'^restaurant/', include('restaurant.urls')),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
